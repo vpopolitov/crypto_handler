a87ee2fe3d549f535f5bc17745afccd9e67d528f
vpopolitov <tiermes@yandex.ru>
2015-02-05 03:15:11 +0400

    byteflow should be inlined

--- app/controllers/messaging_controller.rb
+++ app/controllers/messaging_controller.rb
@@ -1,6 +1,6 @@
 class MessagingController < ApplicationController
   def send_message
     #send_file '/home/pva/Downloads/inhibited-island.mp4', stream: true
-    send_file 'public/inhibited-island.mp4', stream: true
+    send_file 'public/inhibited-island.mp4', :stream => true, :disposition => 'inline'
   end
 end

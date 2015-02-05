99974da52bbd7bebce1f7c726f5c6d9d3a8603fb
vpopolitov <tiermes@yandex.ru>
2015-02-06 01:53:41 +0400

    switch to http live streaming

--- app/controllers/messaging_controller.rb
+++ app/controllers/messaging_controller.rb
@@ -1,6 +1,21 @@
 class MessagingController < ApplicationController
+  include ActionController::Live
+  
+  FILE_NAME = 'public/inhibited-island.mp4'
+  CHUNK_SIZE = 100_000
+  
   def send_message
-    #send_file '/home/pva/Downloads/inhibited-island.mp4', stream: true
-    send_file 'public/inhibited-island.mp4', :stream => true, :disposition => 'inline'
+    #send_file FILE_NAME, :stream => true, :disposition => 'inline'
+    
+    File.open(FILE_NAME) do |f|
+      #send_data(f.read)
+      
+      buffer = ''
+      while buffer = f.read(CHUNK_SIZE)
+        response.stream.write buffer
+      end
+    end
+    
+    response.stream.close
   end
 end

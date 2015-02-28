b59fe828775e9a5608b0555c336aafa8642b27f1
vpopolitov <tiermes@yandex.ru>
2015-02-28 23:55:07 +0300

    add remember token to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227211929) do
+ActiveRecord::Schema.define(version: 20150228194441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150227211929) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "remember_token"
   end
 
   create_table "videos", force: true do |t|

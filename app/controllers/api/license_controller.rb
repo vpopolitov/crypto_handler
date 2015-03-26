a1b8ca01abbf8686acb3b68991eb3f201e229bc0
vpopolitov <tiermes@yandex.ru>
2015-03-26 23:45:39 +0300

    clear-key server with keys storage

--- app/controllers/api/license_controller.rb
+++ app/controllers/api/license_controller.rb
@@ -1,19 +1,16 @@
 class Api::LicenseController < Api::ApiController
   skip_before_filter :restrict_access, only: [:manifest, :map, :token]
 
-  def get
-    keys = {
-        '390eb73a2b0143faa4f036466c073e81' => '3a2a1b68dd2bd9b2eeb25e84c4776668',
-        '3c9592caac1c4bda86b4a747367c4113' => '07e4d653cfb45c66158d93ffce422907'
-    }
+  KEYS_STORAGE_NAME = 'keys-storage.json'
 
+  def get
     key_ids = request.query_string.split('&').inject(Hash.new { |h, k| h[k] = [] }) do |memo, s|
       arr = s.split('=')
       memo[arr[0]].push arr[1]
       memo
     end
 
-    mapped_keys = Hash[keys.map { |k, v| [k.gsub('=', ''), v] }]
+    mapped_keys = Hash[keys_storage.map { |k, v| [k.gsub('=', ''), v] }]
     key_ids
     jwk_array = key_ids['keyid'].inject([]) do |memo, key_id|
       jwk = {
@@ -26,5 +23,12 @@ class Api::LicenseController < Api::ApiController
     end
 
     render json: { keys: jwk_array }, status: :ok
+  end
+
+  private
+
+  def keys_storage
+    @keys_storage ||= JSON.load(
+        ENV["KEYS_STORAGE"] || File.open(Rails.root.join(KEYS_STORAGE_NAME)))
   end
 end
